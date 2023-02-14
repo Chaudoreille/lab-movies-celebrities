ac287c7bd594f76c81e4afc6ebd15f5c731068ac
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-14 17:27:23 +0100

    [IT. 6,7] - Movies creation and listing - done

---
+++ models/Movie.model.js
@@ -0,0 +1,21 @@
+//  Add your code here
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+    },
+    plot: {
+        type: String,
+    },
+    cast: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Celebrity",
+    },
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,41 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+router.get("/", (req, res, next) => {
+    Movie.find()
+        .populate("cast")
+        .then((movies) => {
+            res.locals.movies = movies;
+            res.render("movies/movies");
+        })
+        .catch((error) => {
+            next(error);
+        });
+});
+
+router.get("/create", (req, res, next) => {
+    Celebrity.find()
+        .then((celebrities) => {
+            res.locals.cast = celebrities;
+            res.render("movies/new-movie");
+        })
+        .catch((error) => {
+            next(error);
+        });
+});
+
+router.post("/create", (req, res, next) => {
+    Movie.create(req.body)
+        .then((movie) => {
+            res.redirect("/movies");
+        })
+        .catch((error) => {
+            next(error);
+        });
+});
 
 module.exports = router;
