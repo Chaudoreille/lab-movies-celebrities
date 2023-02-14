75084d252f14ca3b42af766c4a815db907414d30
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-14 16:29:26 +0100

    [IT 3] - add page create celebrity

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,11 +1,19 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
 const celebritySchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+    },
     occupation: {
         type: String,
         default: "unknown",
     },
-    catchPhrase: String,
+    catchPhrase: {
+        type: String,
+        required: true,
+    },
 });
 
-export const Celebrity = mongoose.model("Celebrity", celebritySchema);
+module.exports = mongoose.model("Celebrity", celebritySchema);
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,22 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", (req, res, next) => {
+    Celebrity.create(req.body)
+        .then((celebrity) => {
+            // res.send(req.body);
+            res.redirect("/celebrities");
+        })
+        .catch((error) => {
+            next(error);
+        });
+});
 
 module.exports = router;
